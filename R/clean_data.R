ce884c2b468ec95e043b48a685dab3731f2e7393
Vijaybarve <vbarve@flmnh.ufl.edu>
2017-09-21 23:00:21 -0400

    V 0.0.1 initial commit

---
+++ R/clean_data.R
@@ -0,0 +1,107 @@
+#' Data cleaning according to configuration supplied
+#'
+#' Use \code{get_config} to generate configuration and pass it to this
+#' function to process the data accordingly.
+#'
+#'@param bddata biodiversity data in a dat.frame
+#'@param config configuration generated using \code{get_config}
+#'@param verbose Verbose output if TRUE else brief output if FALSE
+
+#'
+#'@return data frame with clean data
+#'
+#'@examples \dontrun{
+#'library(rgbif)
+#'occdat1 <- occ_data(
+#'  country = "AU",     # Country code for australia
+#'  classKey= 359,      # Class code for mammalia
+#'  limit=5000,         # Get only 5000 records
+#'  )
+#'  myData<-occdat1$data
+#'  myConfig <- get_config()
+#'  cleanData <- clean_data(myData,myConfig)
+#'}
+#'
+#'@export
+clean_data <- function(bddata,config,verbose=T){
+  if(verbose){
+    cat("\n Initial records ...",dim(bddata)[1],"\n")
+  }
+
+  for (i in 1:dim(config)[1]){
+    cat(paste("\n",config$quest[i],config$response[i],"\n"))
+    res <- as.character(config$response[i])
+    #print(res)
+    switch(as.character(config$quest[i]),
+           taxoLevel = {bddata <- taxoLevel(bddata, res)},
+           misNames = {bddata <- misNames(bddata, res)},
+           spatialResolution = {bddata <- spatialResolution(bddata, res)},
+           earliestDate = {bddata <- earliestDate(bddata, res)},
+           temporalResolution = {bddata <- temporalResolution(bddata,res)}
+    )
+    if(verbose){
+      cat("\n Records remaining...",dim(bddata)[1],"\n")
+    }
+  }
+
+  return(bddata)
+}
+
+# Support functions that are called within main function
+
+taxoLevel <- function(bddata, res="SPECIES"){
+  ranks <- c("CLASS", "ORDER", "FAMILY", "GENUS", "SPECIES", "SUBSPECIES")
+  if(!(res %in% ranks)){
+    print("Rank Value unknown. It should be FAMILY, GENUS, SPECIES or SUBSPECIES")
+    return(bddata)
+  }
+  idx <- which(ranks==res)
+  cat(paste("\n Removing records above :",res,"\n"))
+  retmat <- NULL
+  if(idx > 0){
+    for(i in idx:length(ranks)){
+      resmat <- bddata[which(bddata$taxonRank==ranks[i]),]
+      retmat <- rbind(retmat,resmat)
+    }
+  }
+  return(retmat)
+}
+
+misNames <- function(bddata, res="No"){
+  cat("\n fxn misNames not implemented yet \n")
+  return(bddata)
+}
+
+spatialResolution <- function(bddata, res=100){
+  #print("fxn Spatial Resoultion")
+  res <- as.numeric(res)
+  if(res>0){
+    retmat <- bddata[which(bddata$coordinateUncertaintyInMeters<res),]
+  }
+  return(retmat)
+}
+
+earliestDate <- function(bddata, res="1700-01-01"){
+  bddata <- as.data.frame(bddata)
+  ed <- try( as.Date( res, format= "%Y-%m-%d" ) )
+  if( class( ed ) == "try-error" || is.na( ed ) ) {
+    print( "That date wasn't correct!" )
+    return(bddata)
+  }
+  retmat <- bddata[which(as.Date(bddata$eventDate)>ed),]
+  return(retmat)
+}
+
+temporalResolution <- function(bddata,res="Day"){
+  bddata <- as.data.frame(bddata)
+  if(res=="Day"){
+    retmat <- bddata[which(!is.na(bddata$day)),]
+  }
+  if(res=="Month"){
+    retmat <- bddata[which(!is.na(bddata$month)),]
+  }
+  if(res=="Year"){
+    retmat <- bddata[which(!is.na(bddata$year)),]
+  }
+  return(retmat)
+}
---
+++ R/get_config.R
@@ -0,0 +1,86 @@
+#' Get user inputs about data cleaning protocol to follow
+#'
+#' \code{get_config} asks user a set of questions and the answers are stoted
+#' in configuration variable. This variable will be used to clean the data
+#' depending on the responces
+#'
+#'@importFrom utils menu
+#'
+#'@param quest data.frame containing set of questions to be asked {not implimented yet}
+#'@param options list of data frames with options for each questions {not implimented yet}
+#'
+#'@return data frame with the responces
+#'
+#'@examples \dontrun{
+#' myConfig <- get_config()
+#'}
+#'
+#'@export
+
+get_config <- function(quest=NULL,options=NULL){
+  if (is.null(quest)){
+    quest <- data.frame(qvar=c("taxoLevel", "misNames", "spatialResolution", "dateCheck",
+                               "earliestDate", "temporalResolution"),
+                        question=c("What is the lowest taxonomic level you require in your data? ",
+                                   "What you want to do with data with mismatched names? ",
+                                   "What is the spatial resolution required for your data ",
+                                   "Do you care about dates of your observations",
+                                   "What is the earliest date of your observations in this data set (date format-> %Y-%m-%d) ",
+                                   "What temporal resolution are you interested in? "),
+                        rtype=c("I_Numeric","I_Numeric","Numeric","I_Numeric","Date","I_Numeric"),
+                        rlimit=c(4,2,0,2,0,3),
+                        mtype=c("m","m","v","m","v","m"),stringsAsFactors = F)
+  }
+  if (is.null(options)){  options<-list(taxoLevel=data.frame(choice=c(1,2,3,4),value=c("SUBSPECIES", "SPECIES","GENUS","FAMILY")),
+                misNames=data.frame(choice=c(1,2),value=c("Yes","No")),
+                spatialResolution=NULL,
+                dateCheck=data.frame(choice=c(1,2),value=c("Yes","No")),
+                earliestDate=NULL,
+                temporalResolution=data.frame(choice=c(1,2,3),value=c("Year","Month","Day")))
+  }
+  quest1<-(quest)
+  res<-vector(mode="character",length = nrow(quest))
+  for (i in 1:nrow(quest)){
+    if(quest$mtype[i]=="m"){
+      rval <- menu(options[[quest[i,1]]]$value,title = quest[i,2] )
+    } else {
+      rval <- readline(prompt=quest[i,2])
+    }
+    # --- Needs to be checked. Directly borrowed form Ashwin ---
+    if(quest[i,3]=="Numeric"){
+      res[i]=rval
+    }
+    if(quest[i,3]=="Date"){
+      if(IsDate(rval)){
+        res[i]=rval
+      }
+      else{
+        stop(paste("The input in wrong or the date format is not correct"))
+      }
+    }
+
+    if(quest[i,3]=="I_Numeric"){
+
+      if(rval<=quest[i,4] && rval>=1){
+        qvar<-quest[i,1]
+        data<-as.data.frame(options[qvar])
+        colnames(data)<-c("choice","value")
+        res[i]<-as.character((data[data$choice==rval,2]))
+        quest1[i,3]="Numeric"
+      }
+      else{
+        stop(paste("The entered choice is wrong."))
+      }
+    }
+    # --- upto here ---
+  }
+  response<-data.frame(quest=quest1[,1],response=res)
+  return(response)
+}
+
+
+# Functions for input.
+IsDate <- function(mydate, date.format = "%Y-%m-%d") {
+  tryCatch(!is.na(as.Date(mydate, date.format)),
+           error = function(err) {FALSE})
+}
