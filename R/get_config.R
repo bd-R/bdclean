d8cea91aa5852a83dafaa4dcae24d32522d75371
Thiloshon <thiloshon@gmail.com>
2018-05-21 23:59:57 +0530

    updating date in DESCRIPTION

--- R/get_config.R
+++ R/get_config.R
@@ -15,53 +15,59 @@
 #'}
 #'
 #'@export
-get_config <- function(quest=NULL){
-  data(quest, envir=environment())
-  data(responses, envir=environment())
-  quest1<-(quest)
-  res<-vector(mode="character",length = nrow(quest))
-  for (i in 1:nrow(quest)){
-    if(quest$qlink[i]>0){
-      if(res[quest$qlink[i]]!=quest$rescond[i]){
-        next
-      }
+get_config <- function(quest = NULL) {
+    data(quest, envir = environment())
+    data(responses, envir = environment())
+    quest1 <- (quest)
+    res <- vector(mode = "character", length = nrow(quest))
+    for (i in 1:nrow(quest)) {
+        if (quest$qlink[i] > 0) {
+            if (res[quest$qlink[i]] != quest$rescond[i]) {
+                next
+            }
+        }
+        if (quest$mtype[i] == "m") {
+            rval <- menu(responses[[quest[i, 1]]]$value, title = quest[i, 2])
+        } else {
+            rval <- readline(prompt = quest[i, 2])
+        }
+        if (quest[i, 3] == "Numeric") {
+            res[i] = rval
+        }
+        if (quest[i, 3] == "Date") {
+            if (IsDate(rval)) {
+                res[i] = rval
+            }
+            else{
+                stop(paste(
+                    "The input in wrong or the date format is not correct"
+                ))
+            }
+        }
+        if (quest[i, 3] == "I_Numeric") {
+            if (rval <= quest[i, 4] && rval >= 1) {
+                qvar <- quest[i, 1]
+                data <- as.data.frame(responses[qvar])
+                colnames(data) <- c("choice", "value")
+                res[i] <- as.character((data[data$choice == rval, 2]))
+                quest1[i, 3] = "Numeric"
+            }
+            else{
+                stop(paste("The entered choice is wrong."))
+            }
+        }
     }
-    if(quest$mtype[i]=="m"){
-      rval <- menu(responses[[quest[i,1]]]$value,title = quest[i,2] )
-    } else {
-      rval <- readline(prompt=quest[i,2])
-    }
-    if(quest[i,3]=="Numeric"){
-      res[i]=rval
-    }
-    if(quest[i,3]=="Date"){
-      if(IsDate(rval)){
-        res[i]=rval
-      }
-      else{
-        stop(paste("The input in wrong or the date format is not correct"))
-      }
-    }
-    if(quest[i,3]=="I_Numeric"){
-      if(rval<=quest[i,4] && rval>=1){
-        qvar<-quest[i,1]
-        data<-as.data.frame(responses[qvar])
-        colnames(data)<-c("choice","value")
-        res[i]<-as.character((data[data$choice==rval,2]))
-        quest1[i,3]="Numeric"
-      }
-      else{
-        stop(paste("The entered choice is wrong."))
-      }
-    }
-  }
-  response<-data.frame(quest=quest1[,1],response=res)
-  return(response)
+    response <- data.frame(quest = quest1[, 1], response = res)
+    return(response)
 }
 
 
 # Functions for input.
 IsDate <- function(mydate, date.format = "%Y-%m-%d") {
-  tryCatch(!is.na(as.Date(mydate, date.format)),
-           error = function(err) {FALSE})
+    tryCatch(
+        !is.na(as.Date(mydate, date.format)),
+        error = function(err) {
+            FALSE
+        }
+    )
 }
