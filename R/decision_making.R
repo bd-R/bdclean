1680fca692d71fc24c9553c3aa5ba56b76b65a76
Thiloshon <thiloshon@gmail.com>
2018-06-19 12:10:55 +0530

    Fixing Issues

--- R/decision_making.R
+++ R/decision_making.R
@@ -1,7 +1,16 @@
 cleaning_function <- function(bddata, intensity){
     checkColumns <- grep("bdclean", colnames(bddata))
     checkData <- bddata[, checkColumns]
+    
+    # ------------- Decision Making of Cleaning -------------
+    # Cleaning criteria is binary: Pass, Fail
+    # Records with cleanliness-score 10 for all checks will pass.
+    # Records with cleanliness-score less than 10 in atleast 1 check will fail
+    # ------------- Decision Making of Cleaning -------------
+    
     failedData <- which(rowSums(checkData != 10) >= 1)
     
-    return(bddata[!passedData, !checkColumns])
+    # ------------- End of Decision Making of Cleaning -------------
+    
+    return(bddata[!failedData, !checkColumns])
 }
